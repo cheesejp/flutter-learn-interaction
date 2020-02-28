38f9a14a68eccb8105dff4279b009f3a42e71570
Kenji Sonohara <k_sonohara@ewings.biz>
2020-02-28 11:58:55 +0900

    Favoriteボタンを動くようにした。タップすると囲いがでるボタンを作成した。

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,9 @@
 // ==========================================================================================================
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(MyTab());
 
-class MyApp extends StatelessWidget {
+class MyTab extends StatelessWidget {
   @override
   Widget build(BuildContext context) => MaterialApp(
         home: LayoutPage(
@@ -70,11 +70,7 @@ class LayoutPage extends StatelessWidget {
               ],
             ),
           ),
-          Icon(
-            Icons.star,
-            color: Colors.red[500],
-          ),
-          const Text('41'),
+          FavoriteWidget(),
         ],
       ),
     );
@@ -102,36 +98,180 @@ class LayoutPage extends StatelessWidget {
       ),
     );
 
+    Widget _highlightButtonSection = Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: <Widget>[
+        HighlightButton(),
+        HighlightButton(),
+      ],
+    );
+
     return Scaffold(
-      body: SafeArea(
-        child: Column(
-          children: <Widget>[
-            // Image.asset(
-            //   'images/lake.jpg',
-            //   width: 600,
-            //   height: 240,
-            //   fit: BoxFit.cover,
-            // ),
-            Container(
-              width: 600,
-              height: 240,
-              child: Image.network(
-                'https://raw.githubusercontent.com/flutter/website/master/examples/layout/lakes/step5/images/lake.jpg',
-                loadingBuilder: (context, child, progress) {
-                  return progress == null
-                      ? FittedBox(
-                          fit: BoxFit.cover,
-                          child: child,
-                        )
-                      : CircularProgressIndicator();
-                },
+      body: SingleChildScrollView(
+        child: SafeArea(
+          child: Column(
+            children: <Widget>[
+              // Image.asset(
+              //   'images/lake.jpg',
+              //   width: 600,
+              //   height: 240,
+              //   fit: BoxFit.cover,
+              // ),
+              Container(
+                width: 600,
+                height: 240,
+                child: Image.network(
+                  'https://raw.githubusercontent.com/flutter/website/master/examples/layout/lakes/step5/images/lake.jpg',
+                  loadingBuilder: (context, child, progress) {
+                    return progress == null
+                        ? FittedBox(
+                            fit: BoxFit.cover,
+                            child: child,
+                          )
+                        : CircularProgressIndicator();
+                  },
+                ),
               ),
-            ),
-            _titleSection,
-            _iconSection,
-            _textSection,
-          ],
+              _titleSection,
+              _iconSection,
+              _textSection,
+              _highlightButtonSection,
+            ],
+          ),
         ),
+      ),
+    );
+  }
+}
+
+class FavoriteWidget extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _FavoriteWidgetState();
+}
+
+class _FavoriteWidgetState extends State<FavoriteWidget> {
+  bool _isFavorited = true;
+  int _favoriteCount = 41;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      children: <Widget>[
+        Container(
+          padding: EdgeInsets.all(0),
+          child: IconButton(
+            icon: _isFavorited ? Icon(Icons.star) : Icon(Icons.star_border),
+            color: Colors.red[500],
+            onPressed: _toggleFavorite,
+          ),
+        ),
+        SizedBox(
+          width: 18,
+          child: Container(
+            child: Text('$_favoriteCount'),
+          ),
+        )
+      ],
+    );
+  }
+
+  void _toggleFavorite() {
+    setState(() {
+      if (_isFavorited) {
+        _favoriteCount -= 1;
+      } else {
+        _favoriteCount += 1;
+      }
+      _isFavorited = !_isFavorited;
+    });
+  }
+}
+
+class HighlightButton extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _HighlightButtonState();
+}
+
+class _HighlightButtonState extends State<HighlightButton> {
+  bool _active = false;
+
+  void _handleTapboxChanged(bool newValue) {
+    setState(() {
+      _active = newValue;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: HighlightButtonTmp(
+        active: _active,
+        onChanged: _handleTapboxChanged,
+      ),
+    );
+  }
+}
+
+class HighlightButtonTmp extends StatefulWidget {
+  HighlightButtonTmp({@required this.active, @required this.onChanged});
+
+  final bool active;
+  final ValueChanged<bool> onChanged;
+
+  @override
+  State<StatefulWidget> createState() => _HighlightButtonTmpState();
+}
+
+class _HighlightButtonTmpState extends State<HighlightButtonTmp> {
+  bool _highlight = false;
+
+  void _onTap() {
+    print('tap');
+    widget.onChanged(!widget.active);
+  }
+
+  void _onTapDown(TapDownDetails details) {
+    print('tap down');
+    setState(() {
+      _highlight = true;
+    });
+  }
+
+  void _onTapUp(TapUpDetails details) {
+    print('tap up');
+    setState(() {
+      _highlight = false;
+    });
+  }
+
+  void _onTapCancel() {
+    print('tap cancel');
+    setState(() {
+      _highlight = false;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: _onTap,
+      onTapDown: _onTapDown,
+      onTapUp: _onTapUp,
+      onTapCancel: _onTapCancel,
+      child: Container(
+        child: Align(
+          child: Text(widget.active ? 'Active' : 'Inactive'),
+          alignment: Alignment.center,
+        ),
+        decoration: BoxDecoration(
+          color: widget.active ? Colors.lightGreen : Colors.grey[600],
+          border: _highlight
+              ? Border.all(color: Colors.teal[700], width: 5.0)
+              : null,
+        ),
+        width: 100.0,
+        height: 100.0,
       ),
     );
   }
